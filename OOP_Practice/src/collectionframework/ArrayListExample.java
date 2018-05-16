0830c5960966ce56f1716c30c3e6fd807167c0ba
maddela venkata krishna kumar <maddelakrishnakumar@gmail.com>
2018-05-16 07:15:44 +0530

    16/5/2018

---
+++ OOP_Practice/src/collectionframework/ArrayListExample.java
@@ -0,0 +1,21 @@
+package collectionframework;
+
+import java.util.ArrayList;
+
+public class ArrayListExample {
+
+	public static void main(String[] args) {
+
+
+		ArrayList l=new ArrayList();
+		System.out.println(l);
+		l.add(20);
+		l.add(34);
+		System.out.println(l);
+		
+		
+		
+
+	}
+
+}
---
+++ OOP_Practice/src/collectionframework/ListExample.java
@@ -0,0 +1,95 @@
+package collectionframework;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListExample {
+
+	public static void main(String[] args) {
+
+       List li=new ArrayList();
+       
+       // add objects to List
+       
+       
+       /*
+        * 
+        */
+       
+       
+       int a;
+
+a=(int)30.0;
+double b;
+b=30;
+
+
+//String s="123";
+
+//int c=Integer.parseInt(s);
+
+//double d=Double.parseDouble(s);
+
+
+
+int x=30;
+Integer i=new Integer(x); // boxing
+int x1=i.intValue();  // un boxing
+
+// Wrapper classes
+
+/*
+ * int ---- Integer
+ * 
+ * char----Charecter
+ * 
+ * long ---Long
+ * short ------Short
+ * 
+ * float-----Float
+ * 
+ * double ----Double
+ * 
+ * booleam ----Boolean
+ */
+
+       
+       
+       // Type Conversion
+       /*
+        * p to p
+        * 
+        * p to Ob
+        * ob to p
+        */
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       li.add(12);
+       li.add(39.0);
+       li.add("krishna");
+       li.add(12);
+       System.out.println(li);
+       // removing
+       
+       
+       
+       // searching
+       
+       
+       
+       // sorting
+		
+		
+		
+		
+
+	}
+
+}
